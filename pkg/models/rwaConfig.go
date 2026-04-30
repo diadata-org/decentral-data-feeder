3567292f59db983b4e879644ef0632a5890372c4
Sonia Yan <sonia.yan@diadata.org>
2026-04-30 12:12:10 -0400

    add new ETF&Stocks

--- pkg/models/rwaConfig.go
+++ pkg/models/rwaConfig.go
@@ -13,30 +13,41 @@ type RWAConfig struct {
 	ETF         []string `json:"ETF"`
 }
 
+type RWAWSConfig struct {
+	HK_Stocks   []string `json:"HK_Stocks"`
+	US_Stocks   []string `json:"US_Stocks"`
+	FX          []string `json:"FX"`
+	Commodities []string `json:"Commodities"`
+	US_ETF      []string `json:"US_ETF"`
+}
+
 type BeloConfig struct {
 	Pairs []string `json:"Pairs"`
 }
 
 func GetRWAConfig(filename string) (c RWAConfig, err error) {
-
 	data, err := utils.ReadFile(filename)
 	if err != nil {
 		return
 	}
-
 	err = json.Unmarshal(data, &c)
 	return
+}
 
+func GetRWAWSConfig(filename string) (c RWAWSConfig, err error) {
+	data, err := utils.ReadFile(filename)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(data, &c)
+	return
 }
 
 func GetBeloConfig(filename string) (c BeloConfig, err error) {
-
 	data, err := utils.ReadFile(filename)
 	if err != nil {
 		return
 	}
-
 	err = json.Unmarshal(data, &c)
 	return
-
 }
